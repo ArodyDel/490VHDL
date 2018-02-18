507613cd3954b25bd2425a84fe0f5752e265afb9
Arody Deleon <arody888@yahoo.com>
2018-02-18 14:28:37 -0500

    BLUETOOTH PROJECT FOLDER

---
+++ 490/Bluetooth/Bluetooth.srcs/sources_1/new/bluetooth.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2018 08:15:39 PM
+// Design Name: 
+// Module Name: bluetooth
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bluetooth(
+
+    );
+endmodule
